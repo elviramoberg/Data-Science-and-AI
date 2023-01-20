0fb5dd9f8cb3240d0e9bae25a287e27eb5c66613
ViktoriaAndersson <vianders@chalmers.se>
2023-01-20 12:33:11 +0100

    comment

--- Assignment-1.py
+++ Assignment-1.py
@@ -2,9 +2,12 @@ import pandas
 import matplotlib.pyplot as plt
 
 df = pandas.read_csv('gdp-per-capita-worldbank.csv')
+
 gdp = df['GDP per capita']
+
 plt.hist(gdp, bins=50)
 plt.show()
 
 
 #hejejej
+#ok
