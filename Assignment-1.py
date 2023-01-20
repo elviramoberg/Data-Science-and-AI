196a41ed1541c7d49eef87c0e8b5a953e55543eb
ViktoriaAndersson <vianders@chalmers.se>
2023-01-20 15:34:09 +0100

    did som chances

--- Assignment-1.py
+++ Assignment-1.py
@@ -2,7 +2,6 @@ import pandas
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 gdp = pandas.read_csv('gdp-per-capita-worldbank.csv')
 life_exp = pandas.read_csv('life-expectancy.csv')
 
@@ -12,11 +11,13 @@ one_year_life = life_exp[life_exp['Year'] == 2020]
 
 
 merge = one_year_gdp.merge(one_year_life, on= ['Entity', 'Code', 'Year'])
+print(merge)
 
 merge.plot.scatter(x= 'GDP per capita, PPP (constant 2017 international $)', y= 'Life expectancy at birth (historical)', c= 'red')
 plt.xlabel('GDP per capita')
 plt.ylabel('Life expectancy')
-plt.title('2020')
+plt.title(' GDP per capita vs life expectancy for 2020')
+
 
 
 plt.show()
