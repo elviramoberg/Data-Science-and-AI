e1d23b3ced76c024d4a5745faf725b5e5a6444d5
ViktoriaAndersson <vianders@chalmers.se>
2023-01-20 12:47:09 +0100

    comment

--- Assignment-1.py
+++ Assignment-1.py
@@ -2,10 +2,13 @@ import pandas
 import matplotlib.pyplot as plt
 
 df = pandas.read_csv('gdp-per-capita-worldbank.csv')
+life_exp = pandas.read_csv('life-expectancy.csv')
+years = life_exp['Life expectancy at birth (historical)']
 
-gdp = df['GDP per capita']
 
-plt.hist(gdp, bins=50)
+gdp = df["GDP per capita, PPP (constant 2017 international $)"]
+
+plt.hist(gdp, bins=30)
 plt.show()
 
 
