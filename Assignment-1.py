ad6ef50c9e387a8c029ff216a2f99ff1232fef2c
ViktoriaAndersson <vianders@chalmers.se>
2023-01-20 15:20:53 +0100

    did som chances

--- Assignment-1.py
+++ Assignment-1.py
@@ -2,36 +2,50 @@ import pandas
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 gdp = pandas.read_csv('gdp-per-capita-worldbank.csv')
 life_exp = pandas.read_csv('life-expectancy.csv')
-years = life_exp['Life expectancy at birth (historical)']
 
 one_year_gdp = gdp[gdp['Year'] == 2020]
-gdp_countries = list(one_year_gdp['Entity'])
-one_year_gdp_list = list(one_year_gdp["GDP per capita, PPP (constant 2017 international $)"])
-#print(len(one_year_gdp_list))
 
 one_year_life = life_exp[life_exp['Year'] == 2020]
-print(one_year_life)
-one_year_life_list = list(one_year_life['Life expectancy at birth (historical)'])
-#print(len(one_year_life_list))
-
-#plt.hist(one_year_gdp, bins=50)
-#plt.show()
 
 
+merge = one_year_gdp.merge(one_year_life, on= ['Entity', 'Code', 'Year'])
 
-filtered_values = np.where((gdp['Year'] == 2020) & (gdp['Entity'] != 'World'))
-#print(filtered_values)
-
-
-dataframe_gdp = pandas.DataFrame(gdp, columns=["country", "code", "year", "GDP per capita"])
-scaled_df = dataframe_gdp[dataframe_gdp["year"] == 2020]
-#print(scaled_df)
-
-#plt.hist(gdp, bins=30)
-#plt.show()
+merge.plot.scatter(x= 'GDP per capita, PPP (constant 2017 international $)', y= 'Life expectancy at birth (historical)', c= 'red')
+plt.xlabel('GDP per capita')
+plt.ylabel('Life expectancy')
+plt.title('2020')
 
 
-#hejejej
-#ok
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# filtered_values = np.where((gdp['Year'] == 2020) & (gdp['Entity'] != 'World'))
+# #print(filtered_values)
+#
+#
+# dataframe_gdp = pandas.DataFrame(gdp, columns=["country", "code", "year", "GDP per capita"])
+# scaled_df = dataframe_gdp[dataframe_gdp["year"] == 2020]
+# #print(scaled_df)
+#
+# #plt.hist(gdp, bins=30)
+# #plt.show()
+#
+#
+# #hejejej
+# #ok
