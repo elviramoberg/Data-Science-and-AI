535a0b81e19c6d7ffd2d2f8a3bcf7105c6f29267
elviramoberg <elviram@chalmers.se>
2023-01-23 10:42:57 +0100

    code is done

--- Assignment-1.py
+++ Assignment-1.py
@@ -3,77 +3,60 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # A
-gdp = pandas.read_csv('gdp-per-capita-worldbank.csv')
+gdp_per_capita = pandas.read_csv('gdp-per-capita-worldbank.csv')
 life_exp = pandas.read_csv('life-expectancy.csv')
-total_gdp = pandas.read_csv('gross-domestic-product.csv')
+gdp_per_capita_2020 = gdp_per_capita[(gdp_per_capita['Year'] == 2020) & (gdp_per_capita['Code']) & (gdp_per_capita['Entity'] != "World")]
+life_exp_2020 = life_exp[(life_exp['Year'] == 2020) & (life_exp['Code']) & (life_exp['Entity'] != "World")]
+merged = gdp_per_capita_2020.merge(life_exp_2020, on= ['Entity', 'Code', 'Year'])
 
-one_year_gdp = gdp[(gdp['Year'] == 2020) & (gdp['Code']) & (gdp['Entity'] != "World")]
-
-one_year_total_gdp = total_gdp[(total_gdp['Year'] == 2020) & (total_gdp['Code']) & (total_gdp['Entity'] != "World")]
-
-one_year_life = life_exp[(life_exp['Year'] == 2020) & (life_exp['Code']) & (life_exp['Entity'] != "World")]
-#print(one_year_life)
-
-merge = one_year_gdp.merge(one_year_life, on= ['Entity', 'Code', 'Year'])
-#print(merge)
-
-total_merge = one_year_total_gdp.merge(one_year_life, on= ['Entity', 'Code', 'Year'])
-#print(total_merge)
-
-merge.plot.scatter(x= 'GDP per capita, PPP (constant 2017 international $)', y= 'Life expectancy at birth (historical)', c= 'red')
-total_merge.plot.scatter(x= 'GDP (constant 2015 US$)', y= 'Life expectancy at birth (historical)', c= 'red')
+merged.plot.scatter(x= 'GDP per capita, PPP (constant 2017 international $)', y= 'Life expectancy at birth (historical)', c= 'red')
 plt.xlabel('GDP per capita')
 plt.ylabel('Life expectancy')
 plt.title(' GDP per capita vs life expectancy for 2020')
+plt.show()
+
+# B
+mean = np.mean(life_exp_2020['Life expectancy at birth (historical)'])
+std = np.std(life_exp_2020['Life expectancy at birth (historical)'])
+mean_and_1std = mean + std
+life_exp_countries = list(life_exp_2020[life_exp_2020['Life expectancy at birth (historical)'] > mean_and_1std]['Entity'])
+
+print(f"Countries with life expectancy higher than one standard deviation above mean: {life_exp_countries}")
 
 
-#B
-mean_std = np.mean(one_year_life['Life expectancy at birth (historical)']) + np.std(one_year_life['Life expectancy at birth (historical)'])
-mean = np.mean(one_year_life['Life expectancy at birth (historical)'])
+# C (we used median on both GDP and life expectancy)
+total_gdp = pandas.read_csv('gross-domestic-product.csv')
+total_gdp_2020 = total_gdp[(total_gdp['Year'] == 2020) & (total_gdp['Code']) & (total_gdp['Entity'] != "World")]
+median_gdp = np.median(total_gdp_2020['GDP (constant 2015 US$)'])
+median_life_exp = np.median(life_exp_2020['Life expectancy at birth (historical)'])
+low_gdp = (total_gdp_2020[(total_gdp_2020['GDP (constant 2015 US$)'] < median_gdp)])
+high_life_exp = (life_exp_2020[(life_exp_2020['Life expectancy at birth (historical)'] > median_life_exp)])
+merged_2 = low_gdp.merge(high_life_exp)
+low_gdp_high_life_exp_countries = list(merged_2['Entity'])
 
-test = one_year_life[one_year_life['Life expectancy at birth (historical)'] > mean_std]
-test1 = list(test['Entity'])
-print(test1)
+print(f"Countries with low GDP but high life expectancy: {low_gdp_high_life_exp_countries}")
 
 
-#C median på gdp och medelvärde på liv
+# D
+high_gdp = (total_gdp_2020[(total_gdp_2020['GDP (constant 2015 US$)'] > median_gdp)])
+low_life_exp = (life_exp_2020[(life_exp_2020['Life expectancy at birth (historical)'] < median_life_exp)])
+merged_3 = high_gdp.merge(low_life_exp)
+high_gdp_low_life_exp_countries = list(merged_3['Entity'])
 
-# std = np.std(one_year_total_gdp['GDP (constant 2015 US$)'])
-# print(std)
+print(f"Countries with high GDP but low life expectancy: {high_gdp_low_life_exp_countries}")
 
-mean_gdp = np.mean(one_year_total_gdp['GDP (constant 2015 US$)'])
-print(mean_gdp)
-
-median_gdp = np.median(one_year_total_gdp['GDP (constant 2015 US$)'])
-print(median_gdp)
-
-test3 = (one_year_total_gdp[(one_year_total_gdp['GDP (constant 2015 US$)'] < median_gdp)])
-test4 = test3['Entity']
-test5 = (one_year_life[(one_year_life['Life expectancy at birth (historical)'] > mean)])
-test6 = test5['Entity']
-total = test3.merge(test5)
-finish = list(total['Entity'])
-print(finish)
-
-# testC = one_year_life[(one_year_life['Life expectancy at birth (historical)'] > mean) & (one_year_total_gdp['GDP (constant 2015 US$)'] < median_gdp)]
-#testC = (one_year_life[one_year_life['Life expectancy at birth (historical)'] > mean]) & (one_year_total_gdp[(one_year_total_gdp['GDP (constant 2015 US$)'] < mean_gdp)])
-#testC1 = list(testC['Entity'])
-#print(testC1)
-
-# D tvärt mot C
-
-test10 = (one_year_total_gdp[(one_year_total_gdp['GDP (constant 2015 US$)'] > median_gdp)])
-test11 = test10['Entity']
-test12 = (one_year_life[(one_year_life['Life expectancy at birth (historical)'] < mean)])
-test13 = test12['Entity']
-total = test10.merge(test13)
-finish = list(total['Entity'])
-print(finish)
 
 # E
+median_gdp_capita = np.median(gdp_per_capita_2020['GDP per capita, PPP (constant 2017 international $)'])
+high_gdp_capita = (gdp_per_capita_2020[(gdp_per_capita_2020['GDP per capita, PPP (constant 2017 international $)'] > median_gdp_capita)])
+low_life_exp = (life_exp_2020[(life_exp_2020['Life expectancy at birth (historical)'] < median_life_exp)])
+merged_4 = high_gdp_capita.merge(low_life_exp)
+high_gdp_capita_low_life_exp_countries = list(merged_4['Entity'])
+
+print(f"Countries with high GDP per capita but low life expectancy: {high_gdp_capita_low_life_exp_countries}")
 
 
-plt.show()
+
 
 
 
